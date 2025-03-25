cb47ceeef531b500233e5cca304167a2396ce09c
leelopesx <lb.lopes04@gmail.com>
2025-03-24 22:48:01 -0300

    aula finalizada

--- aula1.py
+++ aula1.py
@@ -2,5 +2,13 @@
 num = 1.1111
 print(f"O número é {num:.2f}")
 
+#Como saber a classe da variavel
+nome = "Leticia"
+print(type(nome))
 
+#Ctrl + d : duplica a linha
 
+nota = 8
+media = 7
+aprovado = nota>=media
+print(aprovado)

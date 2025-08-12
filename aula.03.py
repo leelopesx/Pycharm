d1e8259c3a788ab1287895f49d2706e2c6a00246
leelopesx <lb.lopes04@gmail.com>
2025-08-11 22:04:31 -0300

    Aula 03

---
+++ aula.03.py
@@ -0,0 +1,14 @@
+nome_alunos = []
+
+while true:
+    nome = input("Nome (aperte enter para sair):")
+    if nome = "":
+        break
+    nome_aluno.append(nome)
+
+nome_alunos.sort
+
+x=0
+while x < len(nome_alunos)
+    print(nome_alunos[x])
+    x+=

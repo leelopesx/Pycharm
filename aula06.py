e00bbc08de9518c8a5e024a7965097d9a697bb3f
leelopesx <lb.lopes04@gmail.com>
2025-10-20 21:13:41 -0300

    aula do dia 13/10/25

--- aula06.py
+++ aula06.py
@@ -20,3 +20,46 @@ print(lista_frase)
 
 #join - operação inversa
 frase = "".join() #Se deixar espaço nas aspas, a string fica toda separada
+
+#Verificando os primeiros ou os últimos caracteres da string
+print(frase.startswith("Hello"))
+print(frase.endswith("!"))
+
+#In - está dentro da frase?
+print("Z" in frase)
+
+#Count - contagem de caracteres escolhido
+print(frase.count("o"))
+
+#Find - mostra se tem o caractere escolhido e sua posição (se aparecer -1, é pq não existe)
+print(frase.find("e"))
+
+#Ajuste
+print(frase.center(30)) #no centro (30 - o espaço todo)
+print(frase.center(30,"-"))
+
+print(frase.ljust(50,"-")) #esquerda
+print(frase.rjust(50,"-")) #direita
+
+#Split - quebra uma string
+print(frase.split()) #Pode indicar onde quer quebrar
+print(frase)
+
+frase_quebrada = frase.split()
+print(frase_quebrada)
+
+#Renomear
+print(frase.replace("World", "mundo"))
+nova_frase = frase.replace("World", "mundo")
+print(nova_frase)
+print(frase)
+
+#Pular linha
+print("Olá mundo", end="\n\n") #2 linhas
+print("Oi")
+
+#Separação
+print('Hello', "World")
+print('Hello', "World", sep=' ')
+print('Hello', "World", sep=',')
+print('Hello', "World", sep="," , end="!")
